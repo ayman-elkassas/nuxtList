8d93bd094a8f003ec67c147e37327dabeac7460f
Ayman Elkassas <aymanelkassas88@gmail.com>
2020-03-30 17:59:39 +0200

    done training demo

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,37 +1,17 @@
 <template>
-  <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
-  </v-app>
+  <div>
+    <h3>This is error page</h3>
+    <h3>{{error.statusCode}} not found</h3>
+  </div>
 </template>
 
 <script>
 export default {
-  layout: 'empty',
-  props: {
-    error: {
-      type: Object,
-      default: null
-    }
-  },
-  data () {
-    return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred'
-    }
-  },
-  head () {
-    const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
-    return {
-      title
+  layout: 'error-layout',
+  props:{
+    error:{
+      type:Object,
+      default:null
     }
   }
 }
